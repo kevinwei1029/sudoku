584ca605877c5e3492916f969281ddad42122c3d
Robert <hkwei0256@gmail.com>
2023-12-04 23:58:30 +0800

    update

--- sudoku.cpp
+++ sudoku.cpp
@@ -32,6 +32,11 @@ t2, t3, t4 = array for testing use
 
 int main(){
     //pt(2, 0, 0);
-    pt(3, 0, 0);
-    cout << "Hello World!\n";
+    //pt(3, 0, 0);
+    //cout << "Hello World!\n";
+    int N = 9;
+    int K = 20;
+    Sudoku* sudoku = new Sudoku(N, K);
+    sudoku->fillValues();
+    sudoku->printSudoku();
 }
