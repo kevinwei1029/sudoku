1ea36f5d3db69c7df750a5cea80cd5ed781f8013
Robert <hkwei0256@gmail.com>
2023-12-04 18:18:52 +0800

    workinig on pt()

--- generate.h
+++ generate.h
@@ -2,3 +2,12 @@
 #include <bits/stdc++.h>
 #include <Windows.h>
 using namespace std;
+
+int sta;
+
+int t2[4][4] = {
+	{0, 0, 2, 4},
+	{1, 0, 3, 2},
+	{0, 1, 4, 0},
+	{2, 3, 0, 0},
+};
--- print.h
+++ print.h
@@ -2,3 +2,22 @@
 #include <bits/stdc++.h>
 #include "generate.h"
 using namespace std;
+
+void pt(int n, int x, int y) {
+	sta = n;
+	for (int i = 0; i < ((n+1)*(n+1) + 2 * n * n); i++) {  //  i < n+1 + (n+1)*n + 2*n*n
+		cout << "_";
+	}
+	cout << "\n";
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			cout << "|";
+			for (int j = 0; j < n; j++) {
+				cout << " " << setw(2) << t2[i][j+k];
+			}
+			cout << " ";
+		}
+		cout << "|";  //  pt a block
+	}
+	cout << "|\n"//  pt a line
+};
